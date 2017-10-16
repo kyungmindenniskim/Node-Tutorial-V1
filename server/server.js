82c9b0d0832459ac697092fc5add438c9ba8ac45
Kyungmin <primetrm@gmail.com>
2017-10-16 23:52:18 +0900

    auth

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ const _ = require('lodash');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -127,6 +128,10 @@ app.post('/users', (req, res) => {
   }).catch((err) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
