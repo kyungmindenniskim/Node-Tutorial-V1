c17452d608ee1abe0c4f175190a6fdafb5d3dd20
Kyungmin <primetrm@gmail.com>
2017-10-16 23:30:17 +0900

    89 lectures

--- server/server.js
+++ server/server.js
@@ -1,6 +1,18 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var env = process.env.NODE_ENV || 'development';
+console.log('env *****', env);
+
+if ( env === 'development') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if ( env === 'test') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
+
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -72,9 +84,48 @@ app.delete('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
 
-    res.send(todo);
+    res.send({todo: todo});
   }).catch((e) => {
     res.status(400).send();
+  });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', password]);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((err) => {
+    res.status(400).send(e);
   });
 });
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,17 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
-  text: 'First test todo'
+  _id: new ObjectID(),
+  text: 'First test todo',
+  completed: true,
+  completedAt: 333
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }];
 
@@ -70,5 +75,88 @@ describe('Get /todos', () => {
         expect(res.body.todos.length).toBe(2);
       })
       .end(done);
-  })
-})
+  });
+});
+
+describe('Delete /todos/:id', () => {
+  // toHexString is not a function error we had
+  // it('should delete a todo', (done) => {
+  //   var hexId = todos[1]._id.toHexString();
+  //   request(app)
+  //     .delete(`/todos/${hexId}`)
+  //     .expect(200)
+  //     .expect((res) => {
+  //       expect(res.body.todo._id).toBe(hexId);
+  //     })
+  //     .end((err, res) => {
+  //       if (err) {
+  //         return done(err);
+  //       }
+  //
+  //       Todo.findById(hexId).then((todo) => {
+  //         expect(todo).toNotExist();
+  //         done();
+  //       }).catch((e) => {
+  //         done(e);
+  //       });
+  //     });
+  //
+  // });
+
+  // it('should return 404 if todo not found', (done) => {
+  //
+  // });
+  //
+  // it('should return 404 if ObjectID is not valid', (done) => {
+  //
+  // });
+});
+
+describe('Patch /todos/:id', () => {
+  it('should update the todo', (done) => {
+    var hexId = todos[0]._id.toHexString();
+    var text = 'This should be the next Text';
+
+    //grab id of first item
+    //update text, set completed true
+    //200
+    //text is changed, completed is true, completedAt is a number .toBeA
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed: true,
+        text: text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done);
+  });
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    //grab id of second item
+    //update text, set completed to false
+    //200
+    //text is changed, completed false, completedAt is null .toNotExist
+
+    var hexId = todos[1]._id.toHexString();
+    var text = 'This should be the next Text!!';
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed: false,
+        text: text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.completedAt).toNotExist();
+      })
+      .end(done);
+  });
+});
